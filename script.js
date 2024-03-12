aa5e1425913a5cb1db07362f5c04cd756636fb40
Raco15684 <marco.stn15@gmail.com>
2024-03-12 17:41:21 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+// SELECCIONAR ELEMENTOS 
+
+let contenidoTitulo = "Mi primera web"
+
+let titulo = document.querySelector(".logo .fuente")
+titulo.innerHTML = contenidoTitulo;
+
+
+//CONDICIONALES
+
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo)
